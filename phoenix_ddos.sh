cec592bff598297a1c644a1a1c35deeb12d82e16
Mr.R0N1Z <50039352+Mr-R0N1Z@users.noreply.github.com>
2019-05-23 14:38:51 +0700

    Add files via upload

---
+++ phoenix_ddos.sh
@@ -0,0 +1,96 @@
+#usr/bin/bash
+clear
+bi='\033[34;1m' #biru
+i='\033[32;1m' #ijo
+pur='\033[35;1m' #purple
+cy='\033[36;1m' #cyan
+me='\033[31;1m' #merah
+pu='\033[37;1m' #putih
+ku='\033[33;1m' #kuning
+# Jangan Recode Ya gayn
+# yang Recode Besok Mati Amin
+echo $ku Sedang Mengdownload
+pkg install figlet -y
+sleep 0.1
+clear
+pkg install bash -y
+sleep 0.1
+clear
+pkg install toilet -y
+sleep 0.1
+clear
+apt update -y
+sleep 0.1
+clear
+apt upgrade -y
+sleep 0.1
+clear
+pkg install curl -y
+sleep 0.1
+clear
+echo $ku 'Script ini di buat oleh R0N1Z'
+echo $ku Tunggu 5detik
+sleep 5
+clear
+echo $me "                         ______ "
+sleep 0.03
+echo $me "              ______,---'__,---' "
+sleep 0.03
+echo $me "          _,-'---_---__,---' "
+sleep 0.03
+echo $me "   /_    (,  ---____', "
+sleep 0.03
+echo $me "  /  ',,   ', ,-' "
+sleep 0.03
+echo $me " ;/)   ,',,_/,' "
+sleep 0.03
+echo $me " | /\   ,.'//\ "
+sleep 0.03
+echo $me " '-' \ ,,'    '. "
+sleep 0.03
+echo $pu "      '',   ,-- '. "
+sleep 0.03
+echo $pu "      '/ / |      ',         _ "
+sleep 0.03
+echo $pu "      //'',.\_    .\\      ,{==>- "
+sleep 0.03
+echo $pu "   __//   __;_'-  \ ';.__,;' "
+sleep 0.03
+echo $pu " ((,--,) (((,------;  '--' "
+sleep 0.03
+echo $pu " '''  '   ''' "
+sleep 0.03
+echo $i 'Phoenix Cyber Army'
+echo $me '==================='
+sleep 0.3
+echo $ku Siapa Nama Kamu Kaka? #bill
+read Nama #gates
+sleep 0.03
+clear
+
+echo $ku Selamat Datang $Nama di Phoenix_DDos
+echo $me Tunggu 5detik...
+sleep 5
+clear
+sleep 0.3
+
+figlet DOWN!
+echo $ku Contoh Cara Pemakaiannya 
+echo $me Tulis IP PORT DAN PAKETNYA SEKALIGUS CONTOHNYA 74.125.68.113 80 1000
+sleep 0.3
+echo $cy 74.125.68.113 ini adalah IP
+sleep 0.3
+echo $i 80 adalah Port
+sleep 0.3
+echo $me 1000 adalah packet
+sleep 0.3
+echo $bi Faham!
+sleep 0.3
+echo $i Tunggu 20detik
+sleep 15
+clear
+echo $i IP PORT PACKET: #0862
+read PORT #71639
+sleep 0.3
+clear
+python2 PDDOS.py $PORT
